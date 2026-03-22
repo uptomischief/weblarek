8de11f755b23c0624d970bc8125617c4ae6ed80b
Sasha <khodinaal@yandex.by>
2026-03-22 23:35:59 +0300

    создание экземпляров в main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,8 @@ import './scss/styles.scss';
 import { Catalog } from './components/base/models/Catalog';
 import { Basket } from './components/base/models/Basket';
 import { Buyer } from './components/base/models/Buyer';
+import { apiProducts } from './utils/data';
+
+const catalog = new Catalog ();
+const basket = new Basket();
+const buyer = new Buyer();
