accf4a1d4ea8e4ba94e1525be5606c57ba7b0e93
Sasha <khodinaal@yandex.by>
2026-03-24 00:17:06 +0300

    тестирование catalog - нормально

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,24 @@ import { Buyer } from './components/base/models/Buyer';
 import { apiProducts } from './utils/data';
 
 const catalog = new Catalog ();
-const basket = new Basket();
-const buyer = new Buyer();
+// const basket = new Basket();
+// const buyer = new Buyer();
+
+// Класс Catalog
+console.log ('Catalog')
+
+catalog.setProducts(apiProducts.items);
+console.log('масиив товаров каталога: ', catalog.getProducts());
+
+const firstProduct = catalog.getProducts()[0];
+if (firstProduct) {
+    console.log('Ищем товар по id: ', catalog.getProduct(firstProduct.id));
+
+    catalog.setSelectedProduct(firstProduct);
+    console.log('выбранный товар: ', catalog.getSelectedProduct());
+}
+
+// Классс Basket
+
+
+// Класс Buyer
