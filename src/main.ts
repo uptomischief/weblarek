fdb4c89c1905d5ea3d0615898a552abc0f9132d0
Sasha <khodinaal@yandex.by>
2026-03-24 00:47:59 +0300

    тестирование basket - нормально

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import { Buyer } from './components/base/models/Buyer';
 import { apiProducts } from './utils/data';
 
 const catalog = new Catalog ();
-// const basket = new Basket();
+const basket = new Basket();
 // const buyer = new Buyer();
 
 // Класс Catalog
-console.log ('Catalog')
+console.log ('Catalog');
 
 catalog.setProducts(apiProducts.items);
 console.log('масиив товаров каталога: ', catalog.getProducts());
@@ -23,6 +23,32 @@ if (firstProduct) {
 }
 
 // Классс Basket
+console.log ('Basket');
 
+console.log('У нас в корзине сейчас товаров...: ', basket.getCount());
+console.log('Товары в корзине сейчас стоят: ', basket.getTotal());
+
+const product1 = catalog.getProducts()[0];
+const product2 = catalog.getProducts()[1];
+
+if (product1) {
+    basket.addItem(product1);
+    console.log('Добавили товар 1 -> количесвто: ', basket.getCount(), 'со стоимостью: ', basket.getTotal());
+}
+
+if (product2) {
+    basket.addItem(product2);
+    console.log('Добавили товар 2 -> количесвто: ', basket.getCount(), 'со стоимостью: ', basket.getTotal());
+}
+
+console.log('товаров в корзине: ', basket.getItems());
+
+if(product1) {
+    basket.removeItem(product1.id);
+    console.log('товар 1 удалили -> количесвто: ', basket.getCount(), 'со стоимостью: ', basket.getTotal());
+}
+
+basket.clear();
+console.log('очистили корзину -> количесвто: ', basket.getCount(), 'со стоимостью: ', basket.getTotal());
 
 // Класс Buyer
