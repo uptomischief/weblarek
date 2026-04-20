2ceaf2b146aeb1cb45256ccf3e1c6d44ee4d44d5
Sasha <khodinaal@yandex.by>
2026-04-20 19:06:45 +0300

    CardBasket.ts реализован

---
+++ src/components/View/CardBasket.ts
@@ -0,0 +1,26 @@
+import { Card } from "./Card";
+import { ensureElement } from "../../utils/utils";
+
+interface ICardBasketActions {
+    onClick: () => void;
+}
+
+export class CardBasket extends Card<{index: number}> {
+    protected _index: HTMLElement;
+    protected _deleteButton: HTMLButtonElement;
+
+    constructor(container: HTMLElement, actions?: ICardBasketActions) {
+        super(container);
+
+        this._index = ensureElement<HTMLElement>('.basket__item-index', container);
+        this._deleteButton = ensureElement<HTMLButtonElement>('.basket__item-delete', container);
+        
+        if (actions?.onClick) {
+            this._deleteButton.addEventListener('click', actions.onClick);
+        }
+    }
+
+    set index(value: number) {
+        this._index.textContent = String(value);
+    }
+}
